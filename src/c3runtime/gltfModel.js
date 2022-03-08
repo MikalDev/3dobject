0cef6ee58a493b02fabbbc8faff29869fb00b669
MikalDev <keg@kindeyegames.com>
2022-03-08 14:15:19 -0800

    feat (rotate) add rotate to GPU model view matrix

--- src/c3runtime/gltfModel.js
+++ src/c3runtime/gltfModel.js
@@ -53,11 +53,34 @@ class GltfModel
         let currentTexture = null;
 
         const vec4 = globalThis.glMatrix3D.vec4;
+        const mat4 = globalThis.glMatrix3D.mat4;
+        const quat = globalThis.glMatrix3D.quat;
 
         const instanceColor = [instanceC3Color.getR(), instanceC3Color.getG(), instanceC3Color.getB(), instanceC3Color.getA()];
 
 
         const finalColor = vec4.create();
+
+        const tmpModelView = mat4.create();
+        // mat4.copy(tmpProjection, this._matP);
+        debugger
+        if (!this.inst.inEditor) {
+            mat4.copy(tmpModelView, renderer._matMV);
+            const xAngle = this.inst.xAngle;
+            const yAngle = this.inst.yAngle;
+            const zAngle = this.inst.zAngle;
+            const rotate = quat.create();
+            quat.fromEuler(rotate, xAngle, yAngle, zAngle);
+            const modelRotate = mat4.create();
+            const view = mat4.create();
+            // mat4.fromQuat(modelRotate, rotate);
+            // mat4.fromRotationTranslation(modelRotate, rotate, [this.inst.x, this.inst.y, this.inst.z]);
+            mat4.fromRotationTranslationScale(modelRotate, rotate, [x,y,z], [1,-1,1]);
+            mat4.lookAt(view, [854/2,480/2,500], [854/2,480/2,0],[0,1,0]);
+            // mat4.multiply(modelRotate, view, modelRotate);
+            mat4.multiply(modelRotate, tmpModelView, modelRotate);
+            renderer.SetModelViewMatrix(modelRotate);
+        }
 
         // Default color
         renderer.SetColor(instanceC3Color);
@@ -129,6 +152,7 @@ class GltfModel
                     }
                     
                     let i3 = i*3;
+                    /*
                     let x0 = x+(v[ind[i3+0]*3+0]);
                     let y0 = y-(v[ind[i3+0]*3+1]);
                     let z0 = z+(v[ind[i3+0]*3+2]);
@@ -138,6 +162,17 @@ class GltfModel
                     let x2 = x+(v[ind[i3+2]*3+0]);
                     let y2 = y-(v[ind[i3+2]*3+1]);
                     let z2 = z+(v[ind[i3+2]*3+2]);
+                    */
+
+                    let x0 = (v[ind[i3+0]*3+0]);
+                    let y0 = (v[ind[i3+0]*3+1]);
+                    let z0 = (v[ind[i3+0]*3+2]);
+                    let x1 = (v[ind[i3+1]*3+0]);
+                    let y1 = (v[ind[i3+1]*3+1]);
+                    let z1 = (v[ind[i3+1]*3+2]);
+                    let x2 = (v[ind[i3+2]*3+0]);
+                    let y2 = (v[ind[i3+2]*3+1]);
+                    let z2 = (v[ind[i3+2]*3+2]);
 
                     renderer.Quad3D2(
                         x0, y0, z0,
@@ -148,6 +183,10 @@ class GltfModel
                         );
                 }
             }
+        }
+        // Restore modelview matrix
+        if (!this.inst.inEditor) {
+            renderer.SetModelViewMatrix(tmpModelView);
         }
     }
 
@@ -167,9 +206,9 @@ class GltfModel
         let dummyMat4Out = mat4.create();
 
         const scale = this.inst.scale;
-        const xScale = this.inst.scale/this.inst.xScale;
-        const yScale = this.inst.scale/this.inst.yScale;        
-        const zScale = this.inst.scale/this.inst.zScale;
+        const xScale = this.inst.scale/(this.inst.xScale == 0 ? 1 : this.inst.xScale);
+        const yScale = this.inst.scale/(this.inst.yScale == 0 ? 1 : this.inst.yScale);        
+        const zScale = this.inst.scale/(this.inst.zScale == 0 ? 1 : this.inst.zScale);
 
         if(parentMat != undefined)
             mat4.copy(node.matrix, parentMat);
@@ -280,9 +319,9 @@ class GltfModel
         const quat = globalThis.glMatrix3D.quat;
         const gltf = this.gltfData;
         const scale = this.inst.scale;
-        const xScale = this.inst.scale/this.inst.xScale;
-        const yScale = this.inst.scale/this.inst.yScale;
-        const zScale = this.inst.scale/this.inst.zScale;
+        const xScale = this.inst.scale/(this.inst.xScale == 0 ? 1 : this.inst.xScale);
+        const yScale = this.inst.scale/(this.inst.yScale == 0 ? 1 : this.inst.yScale);        
+        const zScale = this.inst.scale/(this.inst.zScale == 0 ? 1 : this.inst.zScale);
         this.drawMeshesIndex = -1;
         
         let rotationQuat = quat.create();
@@ -298,7 +337,8 @@ class GltfModel
             cAngle = this.inst.yAngle;
         }
 
-        quat.fromEuler(rotationQuat, 360-aAngle, 360-bAngle, 360-cAngle);
+        // XXX quat.fromEuler(rotationQuat, 360-aAngle, 360-bAngle, 360-cAngle);
+        quat.fromEuler(rotationQuat, 0, 0, 0);
         mat4.fromRotationTranslation(parentMatrix, rotationQuat, [0,0,0])
 
         this.inst.minBB = [0,0,0];
@@ -313,10 +353,10 @@ class GltfModel
         //todo loop over skinned nodes.
         //todo: limit to ones in scene
         // XXX quat.fromEuler(rotationQuat, this.inst.xAngle, this.inst.yAngle, this.inst.zAngle);
-        quat.fromEuler(rotationQuat, this.inst.xAngle, this.inst.yAngle, this.inst.zAngle);
+        // quat.fromEuler(rotationQuat, this.inst.xAngle, this.inst.yAngle, this.inst.zAngle);
+        quat.fromEuler(rotationQuat, 0, 0, 0);
         for(let ii = 0; ii < gltf.skinnedNodes.length; ii++)
         {
-            
             let node = gltf.skinnedNodes[ii];
             node.rotation = rotationQuat;
             
